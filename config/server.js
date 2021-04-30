38bb0f532783103d91aa98f4605b566ee57fb77c
Arthur <maskalenkasth@gmail.com>
2021-04-30 01:00:26 -0300

    commit inicial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "c38c94ec1d387bb15751243ebc6fd645"),
+    },
+  },
+});
